201e6e94bc95a4e41b532225b322927e55636994
InGwonKang <tnqkr3494@naver.com>
2024-08-01 14:30:19 +0900

    chore: MainBanner change

--- src/components/MainBanner.tsx
+++ src/components/MainBanner.tsx
@@ -8,7 +8,7 @@ const BannerWrapper = styled.div`
   overflow: hidden;
 `;
 
-const TestBanner = styled(motion.div)<{ bgColor: string }>`
+const TestBanner = styled(motion.div)<{ bgImage: string }>`
   display: flex;
   flex-direction: column;
   align-items: baseline;
@@ -16,19 +16,35 @@ const TestBanner = styled(motion.div)<{ bgColor: string }>`
   font-size: 26px;
   font-weight: 700;
   color: #ffffff;
-  background-color: ${(props) => props.bgColor};
+  background-image: url(${(props) => props.bgImage});
+  background-size: cover;
+  background-position: center;
   position: absolute;
   width: 100%;
   height: 100%;
   border-radius: 16px;
   cursor: pointer;
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.4); /* 이미지 어두운 효과 */
+    border-radius: 16px;
+    z-index: 1;
+  }
   p {
     margin-left: 64px;
+    z-index: 2;
+    position: relative;
   }
   svg {
     color: white;
     width: 20px;
     height: 20px;
+    z-index: 2;
   }
 `;
 
@@ -39,6 +55,7 @@ const PrevBtn = styled.div`
   align-items: center;
   width: 100%;
   top: 16px;
+  z-index: 2;
 `;
 
 const NextBtn = styled.div`
@@ -48,6 +65,7 @@ const NextBtn = styled.div`
   align-items: center;
   width: 100%;
   bottom: 16px;
+  z-index: 2;
 `;
 
 const variants = {
@@ -69,7 +87,24 @@ const variants = {
 };
 
 const MainBanner = () => {
-  const colors = ["red", "blue", "gray", "pink", "purple", "teal"];
+  const imgs = [
+    "https://images.unsplash.com/photo-1543352632-5a4b24e4d2a6?q=80&w=2825&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://plus.unsplash.com/premium_photo-1667389723440-dbbde959df52?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8JUVCJThGJTg0JUVDJThCJTlDJUVCJTlEJUJEfGVufDB8fDB8fHww",
+    "https://images.unsplash.com/photo-1547132717-0cd6145a2e6b?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8JUVCJThGJTg0JUVDJThCJTlDJUVCJTlEJUJEfGVufDB8fDB8fHww",
+    "https://images.unsplash.com/photo-1508170754725-6e9a5cfbcabf?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fCVFQiU4RiU4NCVFQyU4QiU5QyVFQiU5RCVCRHxlbnwwfHwwfHx8MA%3D%3D",
+    "https://media.istockphoto.com/id/1498534097/ko/%EC%82%AC%EC%A7%84/%ED%86%A0%EB%A7%88%ED%86%A0-%EC%8C%80-%EA%B0%88%EC%9D%80-%EC%87%A0%EA%B3%A0%EA%B8%B0%EB%A5%BC-%EA%B3%81%EB%93%A4%EC%9D%B8-%EC%96%91%EC%83%81%EC%B6%94-%EC%83%90%EB%9F%AC%EB%93%9C%EB%A5%BC-%EA%B3%81%EB%93%A4%EC%9D%B8-%EB%8F%84%EC%8B%9C%EB%9D%BD.webp?b=1&s=170667a&w=0&k=20&c=SZa7TgHqhlLehR7a2WFdzKPTQ65AFUd8_WKYt12ib-U=",
+    "https://media.istockphoto.com/id/2081769995/ko/%EC%82%AC%EC%A7%84/holding-a-bowl-of-chicken-salad.webp?b=1&s=170667a&w=0&k=20&c=DmbIHpKsYYtlraAfOUuvXVpUSyL7RKp_o1f-hgxM2Q0=",
+  ];
+
+  const texts = [
+    ["당뇨병 환자도 즐길 수 있는 건강한 도시락!", "지금 바로 확인하세요"],
+    ["균형 잡힌 영양소로 가득한 도시락!", "건강을 지키는 첫걸음"],
+    ["맛있고 건강한 당뇨병 전용 도시락!", "지금 주문하세요"],
+    ["당뇨병 관리, 쉽고 맛있게!", "식단 계획 고민 끝!"],
+    ["당뇨병에도 맛을 포기하지 마세요!", "건강한 한 끼를 만나보세요"],
+    ["당신의 건강을 위한 특별한 도시락", "정성을 담아 준비했습니다"],
+  ];
+
   const [index, setIndex] = useState(0);
   const [isHovered, setIsHovered] = useState(false);
   const [isLeaving, setIsLeaving] = useState(false);
@@ -77,18 +112,18 @@ const MainBanner = () => {
   useEffect(() => {
     if (!isHovered) {
       const interval = setInterval(() => {
-        setIndex((prevIndex) => (prevIndex + 1) % colors.length);
+        setIndex((prevIndex) => (prevIndex + 1) % imgs.length);
       }, 2000);
       return () => clearInterval(interval);
     }
-  }, [index, isHovered, colors.length]);
+  }, [index, isHovered, imgs.length]);
 
   const handleNext = () => {
     if (isLeaving) {
       return;
     }
     toggleLeaving();
-    setIndex((prevIndex) => (prevIndex + 1) % colors.length);
+    setIndex((prevIndex) => (prevIndex + 1) % imgs.length);
   };
 
   const handlePrev = () => {
@@ -96,10 +131,11 @@ const MainBanner = () => {
       return;
     }
     toggleLeaving();
-    setIndex((prevIndex) => (prevIndex - 1 + colors.length) % colors.length);
+    setIndex((prevIndex) => (prevIndex - 1 + imgs.length) % imgs.length);
   };
 
   const toggleLeaving = () => setIsLeaving((prev) => !prev);
+
   return (
     <BannerWrapper>
       <AnimatePresence
@@ -115,12 +151,12 @@ const MainBanner = () => {
           animate="center"
           exit="exit"
           transition={{ duration: 0.7 }}
-          bgColor={colors[index]}
+          bgImage={imgs[index]}
           onMouseEnter={() => setIsHovered(true)}
           onMouseLeave={() => setIsHovered(false)}
         >
-          <p>무료 CAD 소스가 이렇게 좋다고?</p>
-          <p>킨텍스 도면 보러가기</p>
+          <p>{texts[index][0]}</p>
+          <p>{texts[index][1]}</p>
           <NextBtn>
             <button onClick={handleNext}>
               <IoIosArrowDown />
